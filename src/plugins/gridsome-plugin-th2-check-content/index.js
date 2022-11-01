cb3f7b07380d3b42c0797fcc0fdae18a92f871bd
Nikolay Dorofeev <dorich2000@gmail.com>
2022-11-01 15:12:36 +0400

    [DO-379] Check files only in production

--- src/plugins/gridsome-plugin-th2-check-content/index.js
+++ src/plugins/gridsome-plugin-th2-check-content/index.js
@@ -3,39 +3,41 @@ const {checkContent, checkObjectForUrls} = require('./check')
 const cliProgress = require('cli-progress')
 
 module.exports = function (api){
-    api.loadSource(({ getCollection }) => {
-        const documentsCollection= getCollection('DocPage'),
-          termsCollection= getCollection('Term'),
-          repositoriesCollection= getCollection('Repository'),
-          releasesCollection= getCollection('Release'),
-          topicsCollection= getCollection('Topic')
+    if (process.env.NODE_ENV === 'production'){
+        api.loadSource(({ getCollection }) => {
+            const documentsCollection= getCollection('DocPage'),
+                termsCollection= getCollection('Term'),
+                repositoriesCollection= getCollection('Repository'),
+                releasesCollection= getCollection('Release'),
+                topicsCollection= getCollection('Topic')
 
-        const {contentToCheck, unitedCollection} = prepareData({
-            documentsCollection,
-            termsCollection,
-            repositoriesCollection,
-            releasesCollection,
-            topicsCollection
-        })
-        console.log('Checking content...')
-        console.time('check_content')
-        const progress = new cliProgress.SingleBar({  }, cliProgress.Presets.shades_classic);
-        progress.start(contentToCheck.length, 0)
-        contentToCheck.forEach((node, index) => {
-            checkContent(node)
-            progress.update(index+1)
-        })
-        progress.stop()
-        console.timeEnd('check_content')
+            const {contentToCheck, unitedCollection} = prepareData({
+                documentsCollection,
+                termsCollection,
+                repositoriesCollection,
+                releasesCollection,
+                topicsCollection
+            })
+            console.log('Checking content...')
+            console.time('check_content')
+            const progress = new cliProgress.SingleBar({  }, cliProgress.Presets.shades_classic);
+            progress.start(contentToCheck.length, 0)
+            contentToCheck.forEach((node, index) => {
+                checkContent(node)
+                progress.update(index+1)
+            })
+            progress.stop()
+            console.timeEnd('check_content')
 
-        console.log('Checking urls...')
-        console.time('check_urls')
-        progress.start(unitedCollection.length, 0)
-        contentToCheck.forEach((node, index) => {
-            checkObjectForUrls(node)
-            progress.update(index+1)
+            console.log('Checking urls...')
+            console.time('check_urls')
+            progress.start(unitedCollection.length, 0)
+            contentToCheck.forEach((node, index) => {
+                checkObjectForUrls(node)
+                progress.update(index+1)
+            })
+            progress.stop()
+            console.timeEnd('check_urls')
         })
-        progress.stop()
-        console.timeEnd('check_urls')
-    })
+    }
 }
