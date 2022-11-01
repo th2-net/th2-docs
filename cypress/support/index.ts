bb6bcea07463ddac017b32a5a4ef7b8332ec6aa2
Nikolay Dorofeev <dorich2000@gmail.com>
2022-11-01 11:54:49 +0400

    Tests draft

---
+++ cypress/support/index.ts
@@ -0,0 +1,13 @@
+// @ts-ignore
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            /**
+             * Custom command to select DOM element by data-cy attribute.
+             * @example cy.dataCy('greeting')
+             */
+            dataCy(value: string): Chainable
+        }
+    }
+}
