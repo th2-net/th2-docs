4a9b8521750eeae8c6f5973c3ec2ba8d6f820b83
Nikolay Dorofeev <dorich2000@gmail.com>
2022-11-04 15:17:19 +0400

    Don't use `cy.wait()`

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,22 +3,19 @@ type SitemapNode = {
 }
 
 describe('Check all docs pages', () => {
-  let sitemap: SitemapNode[] = []
   it('Sitemap exists', () => {
     cy.request('http://localhost:8080/sitemap.dev.json')
-        .then(response => {
-          sitemap = response.body
-        })
   })
 
   it('Visit all pages', () => {
-    // Wait for sitemap
-    cy.wait(1000)
-    cy.log(JSON.stringify(sitemap, null, 2))
-    for (let sitemapNode of sitemap){
-      cy.visit('http://localhost:8080' + sitemapNode.path)
-    }
-    cy.visit('http://localhost:8080/404')
-
+    let sitemap: SitemapNode[] = []
+    cy.request('http://localhost:8080/sitemap.dev.json')
+        .then(response => {
+          sitemap = response.body
+          for (let sitemapNode of sitemap){
+            cy.visit('http://localhost:8080' + sitemapNode.path)
+          }
+          cy.visit('http://localhost:8080/404')
+        })
   })
 })
