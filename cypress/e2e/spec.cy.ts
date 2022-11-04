257483002c88a8ac83977dfd01600558275a7bfe
Stanislav <4eu3ep13@mail.ru>
2022-11-04 12:53:53 +0400

    get developed sitemap in tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,20 @@
-describe('Check all docs pages', () => {
+import axios from 'axios'
+
+type SitemapNode = {
+  path: string
+}
+
+describe('Check all docs pages', async () => {
   it('Sitemap exists', () => {
-    cy.request('http://localhost:8080/sitemap.xml')
+    cy.request('http://localhost:8080/sitemap.dev.json')
+
   })
+  const sitemap = await axios.get<SitemapNode[]>('http://localhost:8080/sitemap.dev.json')
+  for (let sitemapNode of sitemap.data){
+    //cy.log(sitemapNode.path) 
+  }
+
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:8080')
   })
 })
