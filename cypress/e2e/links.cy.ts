fc4098045571b927832ec8d3053eb3ab46dbaadd
Stanislav <4eu3ep13@mail.ru>
2022-11-07 04:45:13 +0400

    DO-382 404 links detecting - first attempt

---
+++ cypress/e2e/links.cy.ts
@@ -0,0 +1,51 @@
+export function checkIfEleExists(ele){
+  return new Promise((resolve,reject)=>{
+      /// here if  ele exists or not
+      cy.get('body').find( ele ).its('length').then(res=>{
+          if(res = 0){
+              //// do task that you want to perform
+              cy.get(ele).select('100').wait(2000);
+              resolve();
+          }else{
+              reject();
+          }
+      });
+  })
+}
+
+
+describe('Broken Link', () => {
+
+    beforeEach(() => {
+  
+      cy.viewport(1280, 1000)
+  
+    })
+  
+    it('Find all broken links', () => {
+  
+      cy.visit('http://localhost:8080/1-7/fundamentals/')
+  
+      cy.get('a').each(link => {
+  
+        if (link.prop('href'))
+  
+          cy.request({
+  
+            url: link.prop('href'),
+  
+            failOnStatusCode: true 
+  
+          })
+ checkIfEleExists(#error-404-flag)
+
+}
+
+  
+        cy.log( link.prop('href'))
+  
+      })
+  
+    })
+  
+  })
