d1660a7b99844026e6a253ac44e55655cda668ff
Nikolay Dorofeev <dorich2000@gmail.com>
2022-11-07 11:43:14 +0400

    Refactor `links.cy.ts`

--- cypress/e2e/links.cy.ts
+++ cypress/e2e/links.cy.ts
@@ -1,8 +1,8 @@
-export function checkIfEleExists(ele){
+function checkIfEleExists(ele: string): Promise<void>{
   return new Promise((resolve,reject)=>{
       /// here if  ele exists or not
       cy.get('body').find( ele ).its('length').then(res=>{
-          if(res = 0){
+          if(res === 0){
               //// do task that you want to perform
               cy.get(ele).select('100').wait(2000);
               resolve();
@@ -15,37 +15,19 @@ export function checkIfEleExists(ele){
 
 
 describe('Broken Link', () => {
-
-    beforeEach(() => {
-  
-      cy.viewport(1280, 1000)
-  
-    })
-  
-    it('Find all broken links', () => {
-  
-      cy.visit('http://localhost:8080/1-7/fundamentals/')
-  
-      cy.get('a').each(link => {
-  
-        if (link.prop('href'))
-  
-          cy.request({
-  
-            url: link.prop('href'),
-  
-            failOnStatusCode: true 
-  
-          })
- checkIfEleExists(#error-404-flag)
-
-}
-
-  
-        cy.log( link.prop('href'))
-  
-      })
-  
-    })
-  
-  })
+    beforeEach(() => {
+        cy.viewport(1280, 1000)
+    })
+    it('Find all broken links', () => {
+        cy.visit('http://localhost:8080/1-7/fundamentals/')
+        cy.get('a').each(link => {
+            if (link.prop('href'))
+            cy.request({
+                url: link.prop('href'),
+                failOnStatusCode: true
+            })
+            checkIfEleExists('#error-404-flag')
+            cy.log( link.prop('href'))
+        })
+    })
+})
