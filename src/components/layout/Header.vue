d045a6cea208d99c246b238c9299f2d0cdd2966f
Nikolay Dorofeev <dorich2000@gmail.com>
2022-12-01 18:11:48 +0400

    Header in separate component

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,106 @@
+<template>
+	<v-app-bar
+		fixed
+		app
+		:color="$vuetify.theme.dark ? 'primary darken-4' : 'secondary'"
+		class="top-layout"
+	>
+		<div class="header-container">
+			<g-link to="/" class="logo-container">
+				<img :src="logo" />
+			</g-link>
+			<div class="header-btns">
+				<v-btn dark :href="$static.metadata.githubRepoLink" target="_blank" icon>
+					<v-icon>mdi-github</v-icon>
+				</v-btn>
+				<theme-switcher />
+				<v-btn dark icon v-if="dense" @click="$emit('input', !value)">
+					<v-icon v-if="!value">mdi-menu</v-icon>
+					<v-icon v-else>mdi-close</v-icon>
+				</v-btn>
+			</div>
+		</div>
+	</v-app-bar>
+</template>
+
+<static-query>
+query {
+	metadata {
+		githubRepoLink
+	}
+}
+</static-query>
+
+<script>
+import logo from '../../assets/img/Th2Logo_full_white.png'
+import ThemeSwitcher from "~/components/layout/ThemeSwitcher";
+import Footer from '~/components/layout/Footer'
+
+export default {
+	props: {
+		value: Boolean,
+		dense: Boolean
+	},
+	name: "Header",
+	components: {
+		ThemeSwitcher, Footer
+	},
+	data(){
+		return{
+			logo
+		}
+	}
+}
+</script>
+
+<style scoped lang="scss">
+@import '~/assets/layout.scss';
+.header-btns{
+	max-width: ($max-width / 5);
+	min-width: ($max-width / 5) - 50px;
+	display: flex;
+	justify-content: flex-end;
+}
+.header-container{
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100%;
+	width: 100%;
+}
+.logo-container{
+	max-width: ($max-width / 5);
+	min-width: ($max-width / 5) - 50px;
+	height: 80%;
+	display: flex;
+	justify-content: flex-start;
+
+svg{
+	height: 100%;
+}
+}
+@media screen and (max-width: $window-width-md) {
+	.header-btns{
+		max-width: ($max-width-md / 5);
+		min-width: ($max-width-md / 5) - 30px;
+	}
+	.logo-container{
+		max-width: ($max-width-md / 5);
+		min-width: ($max-width-md / 5) - 30px;
+	}
+}
+
+@media screen and (max-width: $window-width-sm) {
+	.header-btns{
+		max-width: unset;
+		min-width: unset;
+	}
+	.header-container {
+		justify-content: space-between;
+	}
+	.logo-container{
+		max-width: unset;
+		min-width: unset;
+	}
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,29 +1,6 @@
 <template>
 	<div v-resize="onResize">
-
-		<v-app-bar
-			fixed
-			app
-			:color="$vuetify.theme.dark ? 'primary darken-4' : 'secondary'"
-			class="top-layout"
-		>
-			<div class="header-container">
-				<g-link to="/" class="logo-container">
-					<img :src="logo" />
-				</g-link>
-				<search-window :window-size="windowSize" />
-				<div class="header-btns">
-          <v-btn dark :href="$static.metadata.githubRepoLink" target="_blank" icon>
-            <v-icon>mdi-github</v-icon>
-          </v-btn>
-					<theme-switcher />
-					<v-btn dark icon v-if="windowSize.x < 1024" @click="navPanel = !navPanel">
-						<v-icon v-if="!navPanel">mdi-menu</v-icon>
-						<v-icon v-else>mdi-close</v-icon>
-					</v-btn>
-				</div>
-			</div>
-		</v-app-bar>
+		<Header v-model:nav-panel="navPanel" :dense="windowSize.x < 1024" />
 		<v-navigation-drawer
 			v-model="navPanel"
 			v-if="windowSize.x < 1024"
@@ -40,36 +17,21 @@
 	</div>
 </template>
 
-<static-query>
-query {
-  metadata {
-    githubRepoLink
-  }
-}
-</static-query>
-
 <script>
 import '~/assets/layout.scss'
 import '~/assets/scrollbar.scss'
-import logo from '../assets/img/Th2Logo_full_white.png'
+import Header from "../components/layout/Header.vue";
 import ContentTree from "~/components/layout/ContentTree";
-import ThemeSwitcher from "~/components/layout/ThemeSwitcher";
-import SearchWindow from "~/components/layout/SearchWindow";
-import Footer from '~/components/layout/Footer'
 export default {
 	name: "DefaultLayout",
 	components: {
-			SearchWindow,
-			ThemeSwitcher,
-			ContentTree,
-			Footer
+			ContentTree, Header
 		},
 	data () {
 		return {
 			windowSize: { x: 0, y: 0 },
 			navPanel: false,
-			title: 'th2 docs',
-			logo
+			title: 'th2 docs'
 		}
 	},
 	methods:{
@@ -92,57 +54,5 @@ html {
 }
 .v-app-bar.v-app-bar--fixed.top-layout, .top-layout{
 	z-index: 100 ;
-}
-</style>
-
-<style scoped lang="scss">
-@import '~/assets/layout.scss';
-.header-btns{
-	max-width: ($max-width / 5);
-	min-width: ($max-width / 5) - 50px;
-	display: flex;
-	justify-content: flex-end;
-}
-.header-container{
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	height: 100%;
-	width: 100%;
-}
-.logo-container{
-	max-width: ($max-width / 5);
-	min-width: ($max-width / 5) - 50px;
-	height: 80%;
-	display: flex;
-	justify-content: flex-start;
-
-	svg{
-		height: 100%;
-	}
-}
-@media screen and (max-width: $window-width-md) {
-	.header-btns{
-		max-width: ($max-width-md / 5);
-		min-width: ($max-width-md / 5) - 30px;
-	}
-	.logo-container{
-		max-width: ($max-width-md / 5);
-		min-width: ($max-width-md / 5) - 30px;
-	}
-}
-
-@media screen and (max-width: $window-width-sm) {
-	.header-btns{
-		max-width: unset;
-		min-width: unset;
-	}
-	.header-container {
-		justify-content: space-between;
-	}
-	.logo-container{
-		max-width: unset;
-		min-width: unset;
-	}
 }
 </style>
