8361d5a353f81fce664ade4bf0e371d3c5bf17f5
Nikolay Dorofeev <dorich2000@gmail.com>
2022-12-05 14:51:21 +0400

    Display repos on module card

---
+++ src/components/modules/Th2ReposView.vue
@@ -0,0 +1,32 @@
+<template>
+	<v-row no-gutters outlined>
+		<div v-if="repos.box" class="ma-1">
+			<v-btn :href="repos.box.link" target="_blank" outlined class="text-lowercase">
+				<v-icon>mdi-code-tags</v-icon> {{repos.box.name}}
+			</v-btn>
+		</div>
+		<div v-if="repos.grpc" class="ma-1">
+			<v-btn :href="repos.grpc.link" target="_blank" outlined class="text-lowercase">
+				<v-icon>mdi-arrow-top-right-bottom-left</v-icon> {{repos.grpc.name}}
+			</v-btn>
+		</div>
+	</v-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import {ProdModuleNoteRepos} from "../../plugins/gridsome/plugin/th2-modules-index/types";
+export default Vue.extend({
+	name: "Th2ReposView",
+	props: {
+		repos: {
+			type: Object as () => ProdModuleNoteRepos,
+			required: true
+		}
+	}
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/explore/Modules.vue
+++ src/pages/explore/Modules.vue
@@ -9,14 +9,38 @@
 				<v-card-title>
 					{{module.name}}
 				</v-card-title>
-				<v-card-text>
+				<v-card-text v-if="module.description">
 					{{module.description}}
 				</v-card-text>
 				<v-card-actions>
+					<v-spacer/>
 					<v-btn v-if="module.docs" :to="module.docs"
 								 plain class="text-capitalize">
 						<v-icon>mdi-file-document-outline</v-icon> docs
 					</v-btn>
+				</v-card-actions>
+				<v-card-actions v-if="module.prod_repos">
+					<Th2ReposView :repos="module.prod_repos" />
+				</v-card-actions>
+				<v-card-actions class="flex-column align-start" v-if="module.custom_repos">
+					<section v-if="module.custom_repos.core && module.custom_repos.core.length" class="mt-2">
+						<div>Core:</div>
+						<v-btn v-for="repo in module.custom_repos.core" :key="repo.name"
+									 :href="repo.link" outlined class="text-lowercase ma-1">
+							<v-icon>mdi-github</v-icon> {{repo.name}}
+						</v-btn>
+					</section>
+					<section v-if="module.custom_repos.templates && module.custom_repos.templates.length" class="mt-2">
+						<div>Templates:</div>
+						<Th2ReposView v-for="(repos, index) in module.custom_repos.templates" :key="index"
+													:repos="repos" />
+					</section>
+					<section v-if="module.custom_repos.implementations && module.custom_repos.implementations.length" class="mt-2">
+						<div>Implementations:</div>
+						<Th2ReposView v-for="(repos, index) in module.custom_repos.implementations" :key="index"
+													:repos="repos" />
+					</section>
+
 				</v-card-actions>
 			</v-card>
 		</section>
@@ -31,6 +55,18 @@ query {
 				name
 				docs
 				description
+				prod_repos {
+					box {link, name}
+				}
+				custom_repos {
+					core {link, name}
+					templates {
+						box {link, name}
+					}
+					implementations {
+						box {link, name}
+					}
+				}
 			}
 		}
 	}
@@ -44,13 +80,14 @@ query {
 <script lang="ts">
 import {mapMutations} from "vuex";
 import SubsectionsNav from "../../components/layout/SubsectionsNav.vue";
+import Th2ReposView from "../../components/modules/Th2ReposView.vue";
 import {ModuleNote} from "../../plugins/gridsome/plugin/th2-modules-index/types";
 import Vue from "vue"
 
 export default Vue.extend({
 	name: "Modules",
 	components: {
-		SubsectionsNav
+		SubsectionsNav, Th2ReposView
 	},
 	computed: {
 		modules(): ModuleNote[] {
