19455cfd976169036ae57e20b4733e983ac51d23
Nikolay Dorofeev <dorich2000@gmail.com>
2022-12-06 16:32:15 +0400

    Highlight current subsection

--- src/components/layout/SubsectionsNav.vue
+++ src/components/layout/SubsectionsNav.vue
@@ -3,21 +3,30 @@
 		<v-btn v-for="subsection in subsections" :key="subsection.path"
 					 outlined
 					 :to="subsection.followPath"
-					 class="text-capitalize">
+					 class="text-capitalize"
+					 :class="{
+						 'v-btn--active': isSectionActive(subsection.path)
+					 }">
 			{{subsection.title}}
 		</v-btn>
 	</v-btn-toggle>
 </template>
 
 <script lang="ts">
+import Vue from 'vue'
 import {mapGetters} from "vuex";
 
-export default {
+export default Vue.extend({
 	name: "SubsectionsNav",
 	computed: {
 		...mapGetters(['subsections'])
+	},
+	methods: {
+		isSectionActive(basePath: string): boolean{
+			return this.$route.path.startsWith(basePath)
+		}
 	}
-}
+})
 </script>
 
 <style scoped>
