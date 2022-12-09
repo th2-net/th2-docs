88b4179d673bcec192872023c8da343164cd4f55
Nikolay Dorofeev <dorich2000@gmail.com>
2022-12-09 14:57:36 +0400

    InfraViewer draft

---
+++ src/components/apps/InfraViewer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    InfraViewer
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'InfraViewer',
+  setup() {
+    
+  },
+})
+</script>
