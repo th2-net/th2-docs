a98d662e08b378ac02168ca14aae056b07f071ab
Nikolay Dorofeev <dorich2000@gmail.com>
2023-01-24 17:07:06 +0400

    [DO-461] Building index for search engine

---
+++ src/plugins/gridsome/plugin/create-index/index.ts
@@ -0,0 +1,64 @@
+import {GridsomeCollection, } from "../../../types/utils";
+import {markdownToTxt} from 'markdown-to-txt'
+import * as fs from 'fs'
+
+type DocPage = {
+  path: string
+  title: string
+  content: string
+}
+
+type DocPageCollection = GridsomeCollection<DocPage>
+
+const PAGES_IN_FILE = 5
+
+module.exports = (api: any) => {
+  // Construct content trees
+  api.loadSource(({ getCollection, addCollection }: any) => {
+    const gitOpsPages: DocPageCollection = getCollection('GitOpsPage')
+    const testingPages: DocPageCollection = getCollection('TestingPage')
+    const explorePages: DocPageCollection = getCollection('ExplorePage')
+    const modulePages: DocPageCollection = getCollection('ModulePage')
+
+    const allPages = [
+      ...gitOpsPages._collection.data, 
+      ...testingPages._collection.data, 
+      ...explorePages._collection.data, 
+      ...modulePages._collection.data
+    ]
+
+    const indexFiles: DocPage[][] = []
+    const indexFilesCount = Math.ceil(allPages.length / PAGES_IN_FILE) 
+    for (let file = 0; file < indexFilesCount; file++){
+      const pages: DocPage[] = []
+      for (let page = 0; page < PAGES_IN_FILE; page++){
+        const pageContent = allPages[file * PAGES_IN_FILE + page]
+        if (pageContent)
+          pages.push({
+            title: pageContent.title,
+            path: pageContent.path,
+            content: markdownToTxt(pageContent.content)
+          })
+      }
+      indexFiles.push(pages)
+    }
+
+    fs.mkdirSync('./static/__es-index', { recursive: true })
+    fs.writeFileSync(
+      './static/__es-index/meta.json', 
+        JSON.stringify({
+        count: allPages.length,
+        updated_at: new Date(),
+        files: indexFiles.map((file, index) => `index-${index}.json`)
+      }, null, 2), 
+      { encoding: 'utf-8' }
+    )
+    indexFiles.forEach((file, index) => {
+      fs.writeFileSync(
+        `./static/__es-index/index-${index}.json`, 
+          JSON.stringify(file, null, 2), 
+        { encoding: 'utf-8' }
+      )
+    })
+  })
+}
