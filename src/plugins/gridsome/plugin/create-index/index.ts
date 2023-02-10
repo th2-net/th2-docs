6bef269fb05f8ed8f2dbaf9e1dbe410ea455d880
Nikolay Dorofeev <dorich2000@gmail.com>
2023-02-10 12:08:31 +0400

    Save content to algolia on build

--- src/plugins/gridsome/plugin/create-index/index.ts
+++ src/plugins/gridsome/plugin/create-index/index.ts
@@ -1,6 +1,6 @@
 import {GridsomeCollection, } from "../../../types/utils";
 import {markdownToTxt} from 'markdown-to-txt'
-import * as fs from 'fs'
+import algoliasearch from "algoliasearch"
 
 type DocPage = {
   path: string
@@ -10,11 +10,9 @@ type DocPage = {
 
 type DocPageCollection = GridsomeCollection<DocPage>
 
-const PAGES_IN_FILE = 5
-
 module.exports = (api: any) => {
-  // Construct content trees
   api.loadSource(({ getCollection, addCollection }: any) => {
+    // Get collections
     const readmePages: DocPageCollection = getCollection('ReadmePage')
     const docPages: DocPageCollection = getCollection('DocPage')
 
@@ -24,38 +22,23 @@ module.exports = (api: any) => {
       ...readmePages._collection.data
     ]
 
-    const indexFiles: DocPage[][] = []
-    const indexFilesCount = Math.ceil(allPages.length / PAGES_IN_FILE) 
-    for (let file = 0; file < indexFilesCount; file++){
-      const pages: DocPage[] = []
-      for (let page = 0; page < PAGES_IN_FILE; page++){
-        const pageContent = allPages[file * PAGES_IN_FILE + page]
-        if (pageContent)
-          pages.push({
-            title: pageContent.title,
-            path: pageContent.path,
-            content: markdownToTxt(pageContent.content)
-          })
+    //Save information
+    if (process.env.NODE_ENV === 'production')
+      try {
+        const algolia = algoliasearch(
+          process.env.ALGOLIA_APP_ID ?? '',
+          process.env.ALGOLIA_WRITE_API_KEY ?? ''
+        )
+        const index = algolia.initIndex('docs')
+        index
+          .saveObjects(allPages.map(page => ({
+            objectID: page.path,
+            title: page.title,
+            path: page.path,
+            content: markdownToTxt(page.content),
+          })))
+          .wait()
       }
-      indexFiles.push(pages)
-    }
-
-    fs.mkdirSync('./static/__es-index', { recursive: true })
-    fs.writeFileSync(
-      './static/__es-index/meta.json', 
-        JSON.stringify({
-        count: allPages.length,
-        updated_at: new Date(),
-        files: indexFiles.map((file, index) => `index-${index}.json`)
-      }, null, 2), 
-      { encoding: 'utf-8' }
-    )
-    indexFiles.forEach((file, index) => {
-      fs.writeFileSync(
-        `./static/__es-index/index-${index}.json`, 
-          JSON.stringify(file, null, 2), 
-        { encoding: 'utf-8' }
-      )
-    })
+      catch(err){}
   })
 }
