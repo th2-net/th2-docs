2916a34da36d43586e44d436cdfef4690e818b49
Nikolay Dorofeev <dorich2000@gmail.com>
2023-03-01 14:59:59 +0400

    InfraViewer

---
+++ src/components/apps/infra-viewer/SchemaPicker.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+		<v-select v-model="selectedBranch" :items="branches"
+							outlined dense
+							@change="getCRs" :loading="loading.branches" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {fetchCRs, repoListBranches} from "./fetch-infra-schema";
+export default Vue.extend({
+  name: 'InfraSchemaPicker',
+  props: {
+    schemaLink: {
+      type: String,
+      default: 'https://github.com/th2-net/th2-infra-schema-demo'
+    }
+  },
+  data(){
+    return {
+      branches: [] as string[],
+      selectedBranch: undefined as string | undefined,
+			loading: {
+				branches: false,
+				crs: false
+			}
+    }
+  },
+  computed: {
+  },
+  methods: {
+    getRepo(){
+      const ownerAndRepo = this.schemaLink
+        .replace('https://github.com/', '')
+        .split('/')
+      return {
+        owner: ownerAndRepo[0] ?? 'th2-net',
+        name: ownerAndRepo[1] ?? 'th2-infra-schema-demo'
+      }
+    },
+    async fetchBranches(){
+			this.loading.branches = true
+      try {
+        const repo = this.getRepo()
+        const data = await repoListBranches({
+          owner: repo.owner,
+          repo: repo.name
+        })
+        this.branches = data
+      } catch (e){
+        this.branches = []
+      }
+			this.loading.branches = false
+    },
+    async getCRs(){
+			this.loading.crs = true
+			const result = await fetchCRs(
+				this.getRepo(),
+				this.selectedBranch)
+			this.loading.crs = false
+			this.$emit('crs', result)
+    }
+  },
+  created(){
+    this.fetchBranches()
+  },
+  watch:{
+    schemaLink(){
+      this.fetchBranches()
+    },
+		'loading.crs'(value){
+			this.$emit('loading', value)
+		}
+  }
+})
+</script>
